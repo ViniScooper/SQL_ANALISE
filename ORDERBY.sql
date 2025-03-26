c8d652a744bf2967f47e857223e34a2951498efc
ViniScooper <vviniciuslourenco@gmail.com>
2025-03-26 18:37:17 -0300

    ORDERBY

---
+++ ORDERBY.sql
@@ -0,0 +1,17 @@
+--comando  order by
+--
+---
+----
+	SELECT * 
+	FROM sales.products
+	order by price DESC
+	
+	--- ORDENANDO DECRECENTE
+	
+
+
+------------------------
+
+	SELECT DISTINCT *
+	FROM SALES.products
+	ORDER BY PRICE DESC
