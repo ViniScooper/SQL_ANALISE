81981a7aaa688aff9f17369fcf880f44d982ff74
ViniScooper <vviniciuslourenco@gmail.com>
2025-03-26 19:04:00 -0300

    LIMIT

---
+++ LIMIT.sql
@@ -0,0 +1,38 @@
+
+
+---COMANDO LIMIT
+--- SERVE PARA LIMITAR O N°
+---SINTAXE
+
+	select coluna_01,coluna_02,coluna_03
+	from schema_1.tabela
+	where condilção_x = TRUE
+	order by coluna_1
+	limit N -- O NUMERO DE LINHAS QUE A GENTE QUER QUE APARECA NA NOSSA SELECAO
+	;
+	
+	
+-------------------------------------------------
+
+
+		SELECT * 
+		FROM SALES.PRODUCTS
+		LIMIT 10;
+		
+		
+		
+--------------------------------
+
+
+
+		SELECT * 
+		FROM SALES.PRODUCTS
+		ORDER BY PRICE DESC
+		LIMIT 10;
+		
+		
+-------------------------------
+
+
+
+	---------LIMIT COMANDO USADO PARA LIMITAR A BASE DE DADOS
